d5460443fb098d3f32ac88379449193382d5132f
Leo Zhou <LeoZhou@outlook.com>
2024-09-15 21:17:28 -0700

    add background and content page communication

--- chrome/perfContentScript.js
+++ chrome/perfContentScript.js
@@ -1,1 +1,6 @@
-(function(){function b(){var a=new Date;return a.getFullYear()+"-"+(a.getMonth()+1)+"-"+a.getDate()+" "+a.getHours()+":"+a.getMinutes()+":"+a.getSeconds()+":"+a.getMilliseconds()}console.log("%cDocument start: ","color: blue; font-size: 15px;");console.log("%c"+b(),"color: green; font-size: 13px");window.addEventListener("polymer-ready",function(){console.log("%cpolymer-ready:","color: blue; font-size: 15px;");console.log("%c"+b(),"color: green; font-size: 13px");chrome.runtime.sendMessage({name:"polymer-ready"})})})();
+
+chrome.runtime.sendMessage({message: "Hello from content script", data: "Some data"}, function(response) {
+    console.log("Received response from background script:", response.message);
+  });
+
+chrome.
